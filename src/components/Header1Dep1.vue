09e81a4648f54fb82ac6cc824f88fb122f6ccb31
jhy7828 <jhy7828@toobplus.com>
2023-03-09 21:54:03 +0900

    1

---
+++ src/components/Header1Dep1.vue
@@ -0,0 +1,49 @@
+<script setup></script>
+
+<template>
+  <header>
+    <div>
+      <p>(주)오케이다이렉트대부중개 2016-서울강동-00074(대부중개업)</p>
+      <p style="font-size: 24px; font-weight: 700">1661-0670</p>
+    </div>
+    <div>
+      <img src="../static/images/logo.png" />
+      <span>
+        <p>회사소개</p>
+        <p>대출상품</p>
+        <p>대출사례</p>
+      </span>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="sass">
+header
+  background: #4cabb4
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  padding-bottom: 1%
+  div
+    width: 80%
+    display: flex
+    justify-content: space-between
+    align-items: flex-end
+    padding-block: 10px
+    img
+      height: 45px
+      margin: 2px
+    p
+      font-size: 17px
+      font-weight: 600
+      color: white
+      margin: 0
+    span
+      display: flex
+      align-items: flex-end
+      justify-content: space-between
+      p
+        width: 100px
+        text-align: end
+</style>
---
+++ src/components/SectionBanner1.vue
@@ -0,0 +1,150 @@
+<template>
+  <section>
+    <swiper
+      :spaceBetween="30"
+      :pagination="{
+        clickable: true,
+      }"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide>
+        <div>
+          <h3>개인 회생 대출</h3>
+          <p>개인회생자라면 미납회차,</p>
+          <p>기대출이 있어도 진행가능!</p>
+          <p>파산면책자, 신용회복자도 대출 가능!</p>
+        </div>
+        <div class="content">
+          <img src="../static/images/slide_01.png" />
+          <p>대출을 디자인하다! 디자인론</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div>
+          <h3>일반 신용 대출</h3>
+          <p>복잡한 절차없는 상상론 일반신용대출!</p>
+          <p>직장인, 사업자, 주부라면 복잡한 절차없이!</p>
+        </div>
+        <div class="content">
+          <img src="../static/images/slide_01.png" />
+          <p>대출을 디자인하다! 디자인론</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div>
+          <h3>자동차 담보 대출</h3>
+          <p>무입고 무방문!</p>
+          <p>타던차 그대로 시세 100% 대출 가능!</p>
+        </div>
+        <div class="content">
+          <img src="../static/images/slide_01.png" />
+          <p>대출을 디자인하다! 디자인론</p>
+        </div></swiper-slide
+      >
+      <swiper-slide>
+        <div>
+          <h3>부동산 담보 대출</h3>
+          <p>본인명의의 부동산 소유자라면!</p>
+          <p>전,월세 보증금 담보로도 대출가능!</p>
+        </div>
+        <div class="content">
+          <img src="../static/images/slide_01.png" />
+          <p>대출을 디자인하다! 디자인론</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+    <div class="icons">
+      <div>
+        <img
+          src="../static/images/디자인론_이미지-아이콘_09.png"
+        />
+        <p class="small">복잡한 절차없는</p>
+        <p class="large">일반신용대출</p>
+      </div>
+      <div>
+        <img
+          src="../static/images/디자인론_이미지-아이콘_09.png"
+        />
+        <p class="small">복잡한 절차없는</p>
+        <p class="large">일반신용대출</p>
+      </div>
+      <div>
+        <img
+          src="../static/images/디자인론_이미지-아이콘_09.png"
+        />
+        <p class="small">복잡한 절차없는</p>
+        <p class="large">일반신용대출</p>
+      </div>
+      <div>
+        <img
+          src="../static/images/디자인론_이미지-아이콘_09.png"
+        />
+        <p class="small">복잡한 절차없는</p>
+        <p class="large">일반신용대출</p>
+      </div>
+      <div>
+        <img
+          src="../static/images/디자인론_이미지-아이콘_09.png"
+        />
+        <p class="small">복잡한 절차없는</p>
+        <p class="large">일반신용대출</p>
+      </div>
+      <div>
+        <img
+          src="../static/images/디자인론_이미지-아이콘_09.png"
+        />
+        <p class="small">복잡한 절차없는</p>
+        <p class="large">일반신용대출</p>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Pagination],
+    };
+  },
+};
+</script>
+<style scoped lang="sass">
+.swiper-slide
+    height: 400px
+    display: flex
+    align-items: center
+    justify-content: center
+    .content
+        position: relative
+        p
+            position: absolute
+            top: 15px
+            left: 55px
+            font-size: 26px
+            font-weight: 800
+            color: black
+        img
+            width: 400px
+.icons
+    background: #E3F2F3
+    display: flex
+    align-items: center
+    justify-content: center
+    div
+</style>
