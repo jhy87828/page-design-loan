81cbb35010d2ba7e59a28c04b2159a6a584401b6
jhy7828 <jhy7828@toobplus.com>
2023-03-13 16:01:19 +0900

    전화번호 오른쪽 패딩 살짝

--- src/components/Header2Dep1.vue
+++ src/components/Header2Dep1.vue
@@ -18,7 +18,7 @@ export default {
   <header>
     <div>
       <p>(주)오케이다이렉트대부중개 2016-서울강동-00074(대부중개업)</p>
-      <p style="font-size: 24px; font-weight: 700; padding-right: 1%">
+      <p style="font-size: 24px; font-weight: 700; padding-right: 5%">
         1661-0670
       </p>
     </div>
